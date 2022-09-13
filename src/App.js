cf3b2bab4ae0e10444f2e49236fe741908426941
Prashant Singh <thakurprince9910@gmail.com>
2022-09-13 22:41:03 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Charts from "./Component/Charts/Charts";
+
+function App() {
+  return (
+    <div className="App">
+      <Charts />
+    </div>
+  );
+}
+
+export default App;
